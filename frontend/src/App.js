0f22c485f41cbcc2fb63c9a5316a9718183562e6
HumbleGuy047 <92243872+MagneticGlitter@users.noreply.github.com>
2023-08-04 11:11:28 -0700

    enabled node_modules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route exact path="/" element={<Home />} />
+            <Route exact path="/blogs/create" element={<Home />} />
           </Routes>
         </div>
       </Router>
