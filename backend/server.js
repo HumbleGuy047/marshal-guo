edbfcd2935473e57b814e44650aa7c885f995c5e
MagneticGlitter <92243872+MagneticGlitter@users.noreply.github.com>
2023-08-05 02:47:20 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended: true}));
 //============== CORS ==============
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://marshal-guo.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
