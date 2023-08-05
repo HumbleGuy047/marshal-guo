b71cd0c9f71c6b4577a4915353bab5ea2d960d53
MagneticGlitter <92243872+MagneticGlitter@users.noreply.github.com>
2023-08-05 03:54:14 -0700

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,7 +7,14 @@ const Home = () => {
     useEffect(() => {
         const fetchBlogs = async () => {
             // fetch data from express app server
-            const response = await fetch('https://marshal-guo-api.vercel.app/api/blogs');
+            const response = await fetch('https://marshal-guo-api.vercel.app/api/blogs',
+            {
+                method: 'GET',
+                mode: 'cors', // Set the 'mode' to 'cors' to enable CORS
+                headers: {
+                  'Origin': 'https://marshal-guo.vercel.app' // Set the 'Origin' header to your domain
+                }
+            });
             const json = await response.json();
             if (response.ok) {
                 setBlogs(json);
